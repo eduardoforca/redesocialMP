605fe62a4e5c4624f44e7f4e817ab489c02c0f7e
Ana Carolina dos Santos Pergentino <anacpergentino@MacBook-Pro-de-Ana.local>
2017-07-04 21:44:08 -0300

    Documentação padrão doxygen

--- include/lista.h
+++ include/lista.h
@@ -5,17 +5,22 @@
  *  
  *	@author Eduardo Sousa (eduardoforca) Ana Pergentino(AnaPergentino)
  *  @date 4 Jul 2017
- */
+ 
+**/
 
 #ifndef _LISTA_H_
 #define _LISTA_H_
 
-/*	List is defined as a pointer to a struct listNode*/
+/**
+*	List is defined as a pointer to a struct listNode
+**/
 typedef struct listNode* List;
 
-/*	Definition: A generic unordered Linked List
-	Members:	void* value - Pointer to a generic value
-				List next - Pointer to the rest of the list*/
+/**
+* 	@brief A generic unordered Linked List
+* 	@var	void* value - Pointer to a generic value
+*	@var	List next - Pointer to the rest of the list
+**/
 typedef struct listNode {
 	List next;
 	void* value;
@@ -23,30 +28,38 @@ typedef struct listNode {
 
 
 
-/*	Definition: Creates an empty list
-	Return: a pointer to the empty list created by the function */
+/**
+* @brief Creates an empty list
+* @return a pointer to the empty list created by the function 
+**/
 List cria_lista();
 
 
-/*	Definition: Appends a node to the beginning of a list, doesn't modify the list
-	Parameters: List a_list - list where the node will be appended
-				void* value - generic pointer to the value carried by the node
-	Return: a pointer to the head of the list, which is the new node*/
+/**
+* @brief Appends a node to the beginning of a list, doesn't modify the list
+* @param List a_list - list where the node will be appended
+* @param void* value - generic pointer to the value carried by the node
+* @return  a pointer to the head of the list, which is the new node
+**/
 List adiciona_no(List* a_list, void* value);
 
 int tamanho_list(List a_list);
 
-/*	Definition: Removes a node of a list
-	Parameters: List a_list - list where the node will be removed from
-				int index - location of the node
-	Return: 0 - index not on list
-			1 -	node removed succesfully*/
+/**
+* @brief Removes a node of a list
+* @param List a_list - list where the node will be removed from
+* @param int index - location of the node
+* @retval 0 - index not on list
+* @retval	1 -	node removed succesfully
+**/
 int remove_no(List* a_list, int index);
 
 int remove_no_byvalue(List* a_list, void* value);
 
-/*	Definition: Destroys a list from the memory, deallocates all nodes
-	Parameters: List a_list - list to be destroyed*/
+/**
+* @brief Destroys a list from the memory, deallocates all nodes
+* @param List a_list - list to be destroyed
+**/
 void destroi_lista(List a_list);
 
 #endif
